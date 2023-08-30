b237ff2002d3812a8f32f35a7feceb5a841e1cfc
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-08-30 17:36:04 +0900

    2023.08.30
    블로그 글 수정 API 구현하기
    
    기존 PatchMapping -> PutMapping 으로 수정

--- src/main/java/me/ktony/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/ktony/springbootdeveloper/controller/BlogApiController.java
@@ -47,7 +47,7 @@ public class BlogApiController {
         return ResponseEntity.ok().build();
     }
 
-    @PatchMapping("/api/artivles/{id}")
+    @PutMapping("/api/articles/{id}")
     public ResponseEntity<Article> updateArticle(@PathVariable Long id, @RequestBody UpdateArticleRequest request) {
         Article updatedArticle = blogService.update(id, request);
         return ResponseEntity.ok().body(updatedArticle);
