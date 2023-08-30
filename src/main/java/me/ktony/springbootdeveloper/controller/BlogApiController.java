61e77bc9d3be06b4ff3b03a9f3a889caa9a95239
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-08-30 16:46:58 +0900

    2023.08.30
    /api/atricles 패스로 GET 요청이 오면 글 전체를 조회하는 findAll() 메서드를 호출하고 ArticleResponse로 파싱해 body에 담아 클라이언트에게 전송한다.

--- src/main/java/me/ktony/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/ktony/springbootdeveloper/controller/BlogApiController.java
@@ -3,12 +3,17 @@ package me.ktony.springbootdeveloper.controller;
 import lombok.RequiredArgsConstructor;
 import me.ktony.springbootdeveloper.domain.Article;
 import me.ktony.springbootdeveloper.dto.AddArticleRequest;
+import me.ktony.springbootdeveloper.dto.ArticleResponse;
 import me.ktony.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,5 +27,15 @@ public class BlogApiController {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
     }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+        return ResponseEntity.ok().body(articles);
+    }
+
 
 }
