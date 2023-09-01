7bf4a4318b0d73e5f093e2db48452a0992f27dcf
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-09-01 16:27:11 +0900

    2023.09.01
    블로그 글 목록 뷰 구현

---
+++ src/main/java/me/ktony/springbootdeveloper/controller/BlogViewController.java
@@ -0,0 +1,28 @@
+package me.ktony.springbootdeveloper.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.ktony.springbootdeveloper.dto.ArticleListViewResponse;
+import me.ktony.springbootdeveloper.service.BlogService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class BlogViewController {
+
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+        model.addAttribute("articles", articles);
+        return "articleList";
+    }
+
+}
+
