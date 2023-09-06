5f7d9cc1b18a8d26306d08f70ab5b55b390968bb
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-09-06 19:25:52 +0900

    2023.09.06
    CreateAccessTokenRequest
    CreateAccessTokenResponse
    토큰생성 요청 및 응답용 DTO 생성

---
+++ src/main/java/me/ktony/springbootdeveloper/dto/CreateAccessTokenRequest.java
@@ -0,0 +1,12 @@
+package me.ktony.springbootdeveloper.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateAccessTokenRequest {
+
+    private String refreshToken;
+
+}
